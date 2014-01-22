af710bfb5bd2e7649dff3f3dbc5657638fcab840
Brad Lowenstein <blowenstein93@gmail.com>
2014-01-21 22:45:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fifth::Application.config.secret_key_base = '490765a1ee4993a2113f7fc2d45c73f08ed8df88d12d20569695e7a7832e7f2825768c514fd3cd52fc60767b5edbc29767b872082bc1f673e10e3ae03caa1605'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fifth::Application.config.session_store :cookie_store, key: '_fifth_session'
